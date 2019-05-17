798f2d6dcbda8f34f7fcdcd50f104c276ec817af
JoseGIC <jm.perezo@alumnos.urjc.es>
2019-05-17 20:34:22 +0200

    cccc

---
+++ P3GLSL/scr/main.cpp
@@ -0,0 +1,445 @@
+﻿#include "BOX.h"
+#include "auxiliar.h"
+
+#include <windows.h>
+
+#include <gl/glew.h>
+#include <gl/gl.h>
+#define SOLVE_FGLUT_WARNING
+#include <gl/freeglut.h> 
+#include <iostream>
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+
+
+//////////////////////////////////////////////////////////////
+// Datos que se almacenan en la memoria de la CPU
+//////////////////////////////////////////////////////////////
+
+//Matrices
+glm::mat4	proj = glm::mat4(1.0f);
+glm::mat4	view = glm::mat4(1.0f);
+glm::mat4	model = glm::mat4(1.0f);
+
+
+//////////////////////////////////////////////////////////////
+// Variables que nos dan acceso a Objetos OpenGL
+//////////////////////////////////////////////////////////////
+//Por definir
+unsigned int vshader;
+unsigned int fshader;
+unsigned int program;
+
+//Variables Uniform
+int uModelViewMat;
+int uModelViewProjMat;
+int uNormalMat;
+//Atributos
+int inPos;
+int inColor;
+int inNormal;
+int inTexCoord;
+
+int uColorTex;
+int uEmiTex;
+
+//VAO
+unsigned int vao;
+//VBOs que forman parte del objeto
+unsigned int posVBO;
+unsigned int colorVBO;
+unsigned int normalVBO;
+unsigned int texCoordVBO;
+unsigned int triangleIndexVBO;
+
+unsigned int colorTexId;
+unsigned int emiTexId;
+
+int w = 500, h = 500;
+
+//////////////////////////////////////////////////////////////
+// Funciones auxiliares
+//////////////////////////////////////////////////////////////
+//!!Por implementar
+
+
+//Declaración de CB
+void renderFunc();
+void resizeFunc(int width, int height);
+void idleFunc();
+void keyboardFunc(unsigned char key, int x, int y);
+void mouseFunc(int button, int state, int x, int y);
+
+//Funciones de inicialización y destrucción
+void initContext(int argc, char** argv);
+void initOGL();
+void initShader(const char *vname, const char *fname);
+void initObj();
+void destroy();
+
+
+//Carga el shader indicado, devuele el ID del shader
+//!Por implementar
+GLuint loadShader(const char *fileName, GLenum type);
+
+//Crea una textura, la configura, la sube a OpenGL, 
+//y devuelve el identificador de la textura 
+//!!Por implementar
+unsigned int loadTex(const char *fileName);
+
+
+int main(int argc, char** argv)
+{
+	std::locale::global(std::locale("spanish"));// acentos ;)
+
+	initContext(argc, argv);
+	initOGL();
+	initShader("../shaders_P3/shader.v1.vert", "../shaders_P3/shader.v1.frag");
+	initObj();
+
+	glutMainLoop();
+
+	destroy();
+
+
+
+	return 0;
+}
+	
+//////////////////////////////////////////
+// Funciones auxiliares 
+void initContext(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutCreateWindow("Prácticas OGL");
+
+
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+		exit(-1);
+	}
+	const GLubyte *oglVersion = glGetString(GL_VERSION);
+	std::cout << "This system supports OpenGL Version: " << oglVersion << std::endl;
+	
+	glutReshapeFunc(resizeFunc);
+	glutDisplayFunc(renderFunc);
+	glutIdleFunc(idleFunc);
+	glutKeyboardFunc(keyboardFunc);
+	glutMouseFunc(mouseFunc);
+
+
+}
+
+void initOGL()
+{
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+
+	glFrontFace(GL_CCW);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glEnable(GL_CULL_FACE);
+
+
+	proj = glm::perspective(glm::radians(60.0f), 1.0f, 0.1f, 50.0f);
+	view = glm::mat4(1.0f);
+	view[3].z = -6;
+
+
+}
+void destroy()
+{
+	glDetachShader(program, vshader);
+	glDetachShader(program, fshader);
+	glDeleteShader(vshader);
+	glDeleteShader(fshader);
+	glDeleteProgram(program);
+
+	glDeleteTextures(1, &colorTexId);
+	glDeleteTextures(1, &emiTexId);
+}
+
+void initShader(const char *vname, const char *fname)
+{
+	vshader = loadShader(vname, GL_VERTEX_SHADER);
+	fshader = loadShader(fname, GL_FRAGMENT_SHADER);
+
+	program = glCreateProgram();
+	glAttachShader(program, vshader);
+	glAttachShader(program, fshader);
+
+
+	glBindAttribLocation(program, 0, "inPos");
+	glBindAttribLocation(program, 1, "inColor");
+	glBindAttribLocation(program, 2, "inNormal");
+	glBindAttribLocation(program, 3, "inTexCoord");
+
+	glLinkProgram(program);
+
+	int linked;
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (!linked)
+	{
+		//Calculamos una cadena de error
+		GLint logLen;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLen);
+		char *logString = new char[logLen];
+		glGetProgramInfoLog(program, logLen, NULL, logString);
+		std::cout << "Error: " << logString << std::endl;
+		delete[] logString;
+		glDeleteProgram(program);
+		program = 0;
+		exit(-1);
+	}
+
+	uNormalMat = glGetUniformLocation(program, "normal");
+	uModelViewMat = glGetUniformLocation(program, "modelView");
+	uModelViewProjMat = glGetUniformLocation(program, "modelViewProj");
+
+	uColorTex = glGetUniformLocation(program, "colorTex");
+	uEmiTex = glGetUniformLocation(program, "emiTex");
+
+
+	inPos = glGetAttribLocation(program, "inPos");
+	inColor = glGetAttribLocation(program, "inColor");
+	inNormal = glGetAttribLocation(program, "inNormal");
+	inTexCoord = glGetAttribLocation(program, "inTexCoord");
+
+
+}
+void initObj()
+{
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	if (inPos != -1)
+	{
+		glGenBuffers(1, &posVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, posVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 3,
+			cubeVertexPos, GL_STATIC_DRAW);
+
+
+		glVertexAttribPointer(inPos, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inPos);
+	}
+
+	if (inColor != -1)
+	{
+		glGenBuffers(1, &colorVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 3,
+			cubeVertexColor, GL_STATIC_DRAW);
+		glVertexAttribPointer(inColor, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inColor);
+	}
+	if (inNormal != -1)
+	{
+		glGenBuffers(1, &normalVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, normalVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 3,
+			cubeVertexNormal, GL_STATIC_DRAW);
+		glVertexAttribPointer(inNormal, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inNormal);
+	}
+	if (inTexCoord != -1)
+	{
+		glGenBuffers(1, &texCoordVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, texCoordVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 2,
+			cubeVertexTexCoord, GL_STATIC_DRAW);
+		glVertexAttribPointer(inTexCoord, 2, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inTexCoord);
+	}
+
+
+	glGenBuffers(1, &triangleIndexVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleIndexVBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		cubeNTriangleIndex * sizeof(unsigned int) * 3, cubeTriangleIndex,
+		GL_STATIC_DRAW);
+
+	model = glm::mat4(1.0f);
+
+	colorTexId = loadTex("../img/color2.png");
+	emiTexId = loadTex("../img/emissive.png");
+}
+
+GLuint loadShader(const char *fileName, GLenum type)
+{ 
+	unsigned int fileLen;
+	char *source = loadStringFromFile(fileName, fileLen);
+	//////////////////////////////////////////////
+	//Creación y compilación del Shader
+	GLuint shader;
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1,
+		(const GLchar **)&source, (const GLint *)&fileLen);
+	glCompileShader(shader);
+	delete[] source;
+
+	//Comprobamos que se compiló bien
+	GLint compiled;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled)
+	{
+		//Calculamos una cadena de error
+		GLint logLen;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLen);
+		char *logString = new char[logLen];
+		glGetShaderInfoLog(shader, logLen, NULL, logString);
+		std::cout << "Error: " << logString << std::endl;
+		delete[] logString;
+		glDeleteShader(shader);
+		exit(-1);
+	}
+
+
+
+	return shader; 
+}
+
+
+
+unsigned int loadTex(const char *fileName)
+{ 
+	unsigned char *map;
+	unsigned int w, h;
+	map = loadTexture(fileName, w, h);
+	if (!map)
+	{
+		std::cout << "Error cargando el fichero: "
+			<< fileName << std::endl;
+		exit(-1);
+	}
+
+	unsigned int texId;
+	glGenTextures(1, &texId);
+	glBindTexture(GL_TEXTURE_2D, texId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, (GLvoid*)map);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	delete[] map;
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+		GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+
+
+	
+	
+	return texId; 
+}
+
+void renderFunc()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+
+	glViewport(0, 0, w / 2, h );
+	glUseProgram(program);
+	//→ pintado del objeto!!!!
+	glm::mat4 ojoIzq = glm::translate(glm::mat4(1.0), glm::vec3(0.5, 0, 0));
+	glm::mat4 modelView = ojoIzq * view * model;
+	glm::mat4 modelViewProj = proj *modelView;
+	glm::mat4 normal = glm::transpose(glm::inverse(modelView));
+	if (uModelViewMat != -1)
+		glUniformMatrix4fv(uModelViewMat, 1, GL_FALSE,
+			&(modelView[0][0]));
+	if (uModelViewProjMat != -1)
+		glUniformMatrix4fv(uModelViewProjMat, 1, GL_FALSE,
+			&(modelViewProj[0][0]));
+	if (uNormalMat != -1)
+		glUniformMatrix4fv(uNormalMat, 1, GL_FALSE,
+			&(normal[0][0]));
+
+	if (uColorTex != -1)
+	{
+		glUniform1i(uColorTex, 0);
+	}
+	if (uEmiTex != -1)
+	{
+		glUniform1i(uEmiTex, 1);
+	}
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, colorTexId);
+
+	glActiveTexture(GL_TEXTURE0 + 1);
+	glBindTexture(GL_TEXTURE_2D, emiTexId);
+
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, cubeNTriangleIndex * 3,
+		GL_UNSIGNED_INT, (void*)0);
+
+	glViewport(w / 2, 0, w / 2, h);
+	glUseProgram(program);
+	//→ pintado del objeto!!!!
+	glm::mat4 ojoDer = glm::translate(glm::mat4(1.0), glm::vec3(-0.5, 0, 0));
+	modelView = ojoDer * view * model;
+	modelViewProj = proj * modelView;
+	normal = glm::transpose(glm::inverse(modelView));
+	if (uModelViewMat != -1)
+		glUniformMatrix4fv(uModelViewMat, 1, GL_FALSE,
+			&(modelView[0][0]));
+	if (uModelViewProjMat != -1)
+		glUniformMatrix4fv(uModelViewProjMat, 1, GL_FALSE,
+			&(modelViewProj[0][0]));
+	if (uNormalMat != -1)
+		glUniformMatrix4fv(uNormalMat, 1, GL_FALSE,
+			&(normal[0][0]));
+
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, cubeNTriangleIndex * 3,
+		GL_UNSIGNED_INT, (void*)0);
+
+	
+	glutSwapBuffers();
+}
+
+void resizeFunc(int width, int height)
+{
+	w = width;
+	h = height;
+
+
+	//glViewport(width/2, 0, width/2, height);
+	glutPostRedisplay();
+
+}
+
+
+void idleFunc()
+{
+	model = glm::mat4(1.0f);
+	static float angle = 0.0f;
+	angle = (angle > 3.141592f * 2.0f) ? 0 : angle + 0.01f;
+	model = glm::rotate(model, angle, glm::vec3(1.0f, 1.0f, 0.0f));
+
+	glutPostRedisplay();
+}
+
+void keyboardFunc(unsigned char key, int x, int y){}
+void mouseFunc(int button, int state, int x, int y){}
+
+
+
+
+
+
+
+
+
